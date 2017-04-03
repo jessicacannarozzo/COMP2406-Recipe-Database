753c8716fba478874cc7ba1904c7862a56856d42
jessicacannarozzo <jesscannarozzo@cmail.carleton.ca>
2017-04-03 16:37:41 -0400

    added mongo to server

--- server.js
+++ server.js
@@ -4,16 +4,21 @@
 /*    S/N: 101007447 	 	*/
 /************************/
 
-
+var mongo = require('mongodb').MongoClient;
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var hat = require('hat');
 var cookieParser = require('cookie-parser');
 
+var db;
+
 app.set('views','./public');
 app.set('view engine','pug');
 
+app.get("/recipes", function(req, res) {
+	res.send("Lol");
+});
 
 app.use(function(req,res,next){
 	console.log(req.method+" request for "+req.url);
@@ -22,10 +27,16 @@ app.use(function(req,res,next){
 
 app.use(cookieParser());
 //serve main page + credentials
-app.get(['/', '/index.html', '/home', '/index'], function(req,res){
+app.get("/", function(req,res){
 
-	res.render('index',{});
+	res.render('index');
 
 });
 
-app.listen(2406,function(){console.log("Server listening on port 2406");});
+mongo.connect("mongodb://localhost:27017/demoDB",function(err,database){
+	// if(err)throw err;
+	app.listen(2406,function(){console.log("Server is alive!!");});
+	db = database; //store the connection (pool)
+});
+
+// app.listen(2406,function(){console.log("Server listening on port 2406");});
