6706811555a09d031f09148dd91ff37b91030c9f
jessicacannarozzo <jesscannarozzo@cmail.carleton.ca>
2017-04-07 11:41:23 -0400

    Checkmark div works

--- server.js
+++ server.js
@@ -19,37 +19,44 @@ app.set('view engine','pug');
 
 //function executed every time the app object receives a request
 app.use(express.static("./public"));
+app.use("/recipe",bodyParser.urlencoded({extended:true}));
+
+
+mongo.connect("mongodb://localhost:27017/recipeDB",function(err,database){
+	if(err)throw err;
+	app.listen(2406,function(){console.log("Server is alive!!");});
+	db = database; //store the connection (pool)
+});
 
 //send recipe list
 app.get("/recipes", function(req, res) {
 	recipeList.names = [];
 	res.writeHead(200, {"Content-Type" : 'application/json'});
-	mongo.connect("mongodb://localhost:27017/recipeDB",function(err,database){
-		if(err)throw err;
-		db = database; //store the connection (pool)
-		db.collection("recipes").find().each(function(err, u) {
-			if (!u) {
-				db.close();
-				res.end(JSON.stringify(recipeList));
-			} else recipeList.names.push(u.name);
-		});
+	db.collection("recipes").find().each(function(err, u) {
+		if (!u) {
+			res.end(JSON.stringify(recipeList));
+		} else recipeList.names.push(u.name); //add to option menu
 	});
 });
 
-app.use("/recipe",bodyParser.urlencoded({extended:true}));
+app.get("/recipe/:name", function(req, res) {
+	db.collection("recipes").findOne({name: req.params.name}, function(err, recipe) {
+		if (err) res.sendStatus(500);
+		if (!recipe) res.sendStatus(404);
+		console.log(JSON.stringify(recipe));
+		res.writeHead(200, {"Content-Type" : 'application/json'});
+		res.end(JSON.stringify(recipe));
+	});
+});
 
 //post/save a new recipe
 app.post("/recipe", function(req, res) {
-	var recipe = new Recipe(req.body.name, req.body.duration, req.body.ingredients, req.body.directions, req.body.notes);
+	var recipe = Recipe(req.body);
 
-	mongo.connect("mongodb://localhost:27017/recipeDB",function(err,db){
-		if(err)throw err;
-		db.collection("recipes").update({name:req.body.name},req.body,{upsert:true, w: 1}, function(err, result) {
-			if (err) res.sendStatus(500); //internal server error/
-			// else if () //400, data missing
-			else res.sendStatus(200); //OK, success.
-			db.close();
-		});
+	db.collection("recipes").update({name: recipe.name}, recipe,{upsert:true, w: 1}, function(err, result) {
+		if (err) res.sendStatus(500); //internal server error/
+		// else if () //400, data missing
+		else res.sendStatus(200); //OK, success.
 	});
 });
 
@@ -66,12 +73,12 @@ app.get("/", function(req,res){
 
 });
 
-app.listen(2406,function(){console.log("Server listening on port 2406");});
-
-function Recipe(name, duration, ingredients, directions, notes) { //create a recipe obj
-	this.name = name;
-	this.duration = duration;
-	this.ingredients = ingredients;
-	this.directions = directions;
-	this.notes = notes;
+function Recipe(options) { //create a recipe obj
+	return {
+		name: options.name,
+		duration: options.duration,
+		ingredients: options.ingredients,
+		directions: options.directions,
+		notes: options.notes
+	}
 }
